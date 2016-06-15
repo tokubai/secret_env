0a5bc657df397dbe9483e1bbef862b4123012ca5
adorechic <adorechic@gmail.com>
2016-06-15 19:27:33 +0900

    Extract storage module file

--- lib/secret_env.rb
+++ lib/secret_env.rb
@@ -1,4 +1,5 @@
-require "secret_env/version"
+require 'secret_env/version'
+require 'secret_env/storage'
 require 'yaml'
 
 module SecretEnv
@@ -33,61 +34,6 @@ module SecretEnv
         parts << part.gsub(scanner.matched, secret)
       end
       parts.join
-    end
-  end
-
-  module Storage
-    class << self
-      def setup(config)
-        if config
-          klass(config.fetch('type')).new(namespace: config['namespace'])
-        else
-          Storage::Plain.new
-        end
-      end
-
-      private
-
-      def klass(type)
-        case type
-        when 'plain'
-          Storage::Plain
-        when 'credstash'
-          Storage::CredStash
-        else
-          raise "Unknown storage type: #{type}"
-        end
-      end
-    end
-
-    class Base
-      attr_reader :namespace
-
-      def initialize(namespace: '')
-        @namespace = namespace
-      end
-
-      def retrieve(secret_key)
-        raise NotImplemedError
-      end
-
-      private
-
-      def full_key(secret_key)
-        "#{namespace}#{secret_key}"
-      end
-    end
-
-    class Plain < Base
-      def retrieve(secret_key)
-        "#\{#{full_key(secret_key)}\}"
-      end
-    end
-
-    class CredStash < Base
-      def retrieve(secret_key)
-        ::CredStash.get(full_key(secret_key))
-      end
     end
   end
 end
